11621f50122811675a0de66dd1fea5d3d3038025
Christoph Fabianek <christoph.fabianek@gmail.com>
2016-05-16 15:04:07 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# app-relationship
-PIA Application to quantify your relationship
+# Beziehungsstatus
+Der [OwnYourData](https://www.ownyourdata.eu) Beziehungsstatus
+
+## Installation
+
+Die App kann gratis über den offiziellen [OwnYourData SAM](http://oyd-sam.herokuapp.com) (Store for Algorithms) installiert werden. Klicke dazu in der PIA App-Liste "Plugin installieren" und wähle "Beziehungs-Status" (ID: eu.ownyourdata.relationship) aus.
+
+Die Beziehungsstatus-App benötigt das Shiny-Host-Service (ebenfalls verfügbar am OwnYourData SAM, ID: eu.ownyourdata.shinyhost) und [Docker](https://www.docker.com/) installiert.
+
+
+## Verwendung
+
+Die App umfasst folgende Funktionen:
+
+* tägliche Datenerfassung
+* Visualisierung der vorhandenen Daten zur Identifikation von Zusammenhängen
+* Einschränkung der Darstellung auf ein bestimmtes Zeitfenster
+* optional: automatisierte Datensammlung
+* optional: wöchentliche Emails die eigenes Befinden und Medikamenteneinnahme abfragen
+
+
+## Für Entwickler  
+
+Diese App wurde in [R](https://cran.r-project.org/) entwickelt und verwendet [Shiny](http://shiny.rstudio.com/). Zur Ausführung wird entweder das OwnYourData Shiny Service benötigt (siehe oben: Installation) oder es existiert ein bereits [installierter Shiny Server](https://github.com/rstudio/shiny-server/wiki/Building-Shiny-Server-from-Source). Wird ein eigener Shiny Server betrieben, kann in der PIA App-Liste mit "Register a new Plugin" das Manifest base64-encodiert hinzugefügt werden (angegeben am Beginn der Datei `server.R`) und in der App unter Konfiguration müssen die Parameter URL, App-Key und App-Secret selbst gesetzt werden.  
+Zum Ausprobieren kann die App auf [Heroku](https://www.heroku.com/) deployed werden:  
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+
+## Verbessere die Kontoentwicklungs-App
+
+1. Fork it!
+2. erstelle einen Feature Branch: `git checkout -b my-new-feature`
+3. Commit deine Änderungen: `git commit -am 'Add some feature'`
+4. Push in den Branch: `git push origin my-new-feature`
+5. Sende einen Pull Request
+
+## Lizenz
+
+MIT Lizenz 2016 - Own Your Data
---
+++ ui.R
@@ -0,0 +1,116 @@
+# Setup and config ========================================
+library(shiny)
+library(shinyBS) #https://ebailey78.github.io/shinyBS/index.html
+library(jsonlite)
+
+source("oyd_helpers.R")
+
+saved <- getPiaConnection('relshp')
+saved_url        <- saved[['url']]
+saved_app_key    <- saved[['app_key']]
+saved_app_secret <- saved[['app_secret']]
+
+# Shiny UI ================================================
+shinyUI(fluidPage(
+  titlePanel("Beziehungsstatus"),
+  bsAlert("topAlert"),
+  sidebarLayout(
+    sidebarPanel(
+            tabsetPanel(type='tabs', selected='Beziehungsstatus',
+# Relationship specific ===========================================
+                        tabPanel('Beziehungsstatus',
+                                 h3('Anzeige'),
+                                 dateRangeInput('dateRange',
+                                                language = 'de',
+                                                separator = ' bis ',
+                                                format = 'dd.mm.yyyy',
+                                                label = 'Zeitfenster',
+                                                start = Sys.Date() - 365, end = Sys.Date()),
+                                 hr(),
+                                 h3('Daten eingeben'),
+                                 dateInput('manDate',
+                                           language = 'de',
+                                           format = 'dd.mm.yyyy',
+                                           label = 'Datum'),
+                                 h4('Allgemein'),
+                                 tags$table(style='width:100%',
+                                            tags$tr(tags$td(checkboxInput('useGeneral1', label = 'Person #1', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manGeneral1', NULL, value=5, min=1, max=10), style='width:100%')),
+                                            tags$tr(tags$td(checkboxInput('useGeneral2', label = 'Person #2', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manGeneral2', NULL, value=5, min=1, max=10), style='width:100%'))),
+                                 h4('Energie'),
+                                 tags$table(style='width:100%',
+                                            tags$tr(tags$td(checkboxInput('useEnergy1', label = 'Person #1', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manEnergy1', NULL, value=5, min=1, max=10), style='width:100%')),
+                                            tags$tr(tags$td(checkboxInput('useEnergy2', label = 'Person #2', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manEnergy2', NULL, value=5, min=1, max=10), style='width:100%'))),
+                                 h4('Gesundheit'),
+                                 tags$table(style='width:100%',
+                                            tags$tr(tags$td(checkboxInput('useHealth1', label = 'Person #1', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manHealth1', NULL, value=5, min=1, max=10), style='width:100%')),
+                                            tags$tr(tags$td(checkboxInput('useHealth2', label = 'Person #2', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manHealth2', NULL, value=5, min=1, max=10), style='width:100%'))),
+                                 h4('Zufriedenheit'),
+                                 tags$table(style='width:100%',
+                                            tags$tr(tags$td(checkboxInput('useSatisfaction1', label = 'Person #1', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manSatisfaction1', NULL, value=5, min=1, max=10), style='width:100%')),
+                                            tags$tr(tags$td(checkboxInput('useSatisfaction2', label = 'Person #2', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manSatisfaction2', NULL, value=5, min=1, max=10), style='width:100%'))),
+                                 h4('Entspannung'),
+                                 tags$table(style='width:100%',
+                                            tags$tr(tags$td(checkboxInput('useRelaxation1', label = 'Person #1', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manRelaxation1', NULL, value=5, min=1, max=10), style='width:100%')),
+                                            tags$tr(tags$td(checkboxInput('useRelaxation2', label = 'Person #2', value = TRUE, width='100px')),
+                                                    tags$td(sliderInput('manRelaxation2', NULL, value=5, min=1, max=10), style='width:100%'))),
+                                 actionButton('exportButton', 'Speichern'),
+                                 htmlOutput('last_saved')
+                        ),
+
+# PIA =====================================================
+                        tabPanel("PIA", 
+                            h3('Authentifizierung'),
+                            textInput('relshp_url', 'Adresse:', saved_url),
+                            textInput('relshp_app_key', 'ID (Beziehungsstatus):', saved_app_key),
+                            textInput('relshp_app_secret', 'Secret (Beziehungsstatus):', saved_app_secret),
+                            checkboxInput("localRelshpSave", label = "Zugriffsinformationen lokal speichern", value = FALSE),
+                            hr(),
+                            htmlOutput("relshp_token"),
+                            htmlOutput("relshp_records")
+                        ),
+
+# Scheduler ===============================================
+                        tabPanel("Benachrichtigung", 
+                                 h3('Benachrichtigung'),
+                                 textInput('email', 'Emailadresse:'),
+                                 htmlOutput("email_status"),
+                                 helpText('Wenn sie hier ihre Emailadressen eingeben, erhalten beide Teilnehmer jedes Wochenende eine Email mit den 5 Fragen.'),
+                                 hr(),
+                                 h3('Email Konfiguration'),
+                                 htmlOutput("mail_config"),
+                                 textInput('mailer_address', 'Mail Server:'),
+                                 numericInput('mailer_port', 'Port:', 0),
+                                 textInput('mailer_user', 'Benutzer:'),
+                                 passwordInput('mailer_password', 'Passwort')
+                        )
+            )
+    ),
+
+# Main Panel ==============================================
+    mainPanel(
+            bsAlert("noData"),
+            tabsetPanel(type="tabs",
+                        tabPanel("Allgemein", 
+                                 plotOutput(outputId = "plotGeneral", height = "300px")),
+                        tabPanel("Energie", 
+                                 plotOutput(outputId = "plotEnergy", height = "300px")),
+                        tabPanel("Gesundheit", 
+                                 plotOutput(outputId = "plotHealth", height = "300px")),
+                        tabPanel("Zufriedenheit", 
+                                 plotOutput(outputId = "plotSatisfaction", height = "300px")),
+                        tabPanel("Entspannung", 
+                                 plotOutput(outputId = "plotRelaxation", height = "300px"))
+            ),
+            bsAlert("noPIA")
+    )
+  )
+))
